081ac8ee8b67c9fae8bc00976ddbe633ff4d8791
Christina Kozanian <christinakozanian@gmail.com>
2024-02-08 12:46:54 -0800

    lab complete

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,18 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, root=None):
+        self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+    def get_element_by_id(self, id):
+        return self._traverse(self.root, id)
+
+    def _traverse(self, node, id):
+        if node['id'] == id:
+            return node
+
+        for child in node['children']:
+            result = self._traverse(child, id)
+            if result:
+                return result
+
+        return None
+
